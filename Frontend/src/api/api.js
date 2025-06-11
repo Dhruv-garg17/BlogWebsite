d1d2d155524b124e2ee4bab9fd58b1ad34b54ea0
Dhruv-garg17 <dhruv1689.be21@chitkara.edu.in>
2025-06-12 00:27:56 +0530

    backend url updated

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL:'https://blogwebsite-2u5t.onrender.com/',
 });
 
 // Request interceptor for adding auth token
